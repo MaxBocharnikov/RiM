e0f9674f30744f230492fdcc61386d3aecfaffa8
= <=>
2026-07-08 21:16:04 +0700

    setup import order prettier config and fix imports all around the app

--- src/_shared/layout/Header/Header.tsx
+++ src/_shared/layout/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router';
+
 import { Logo } from '../../../components';
 
 import './Header.css';
--- src/pages/CharactersList/CharactersList.tsx
+++ src/pages/CharactersList/CharactersList.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router';
+
 import { Loader } from '../../components';
 
 import './CharactersList.css';
