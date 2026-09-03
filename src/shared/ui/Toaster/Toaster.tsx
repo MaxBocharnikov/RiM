9443fee1f49fbee87c72b67141d98096c52b69f2
= <=>
2026-09-03 17:47:53 +0400

    adjust Toaster

--- src/shared/ui/Toaster/Toaster.tsx
+++ src/shared/ui/Toaster/Toaster.tsx
@@ -1,8 +1,3 @@
 import { Toaster as HotToaster } from 'react-hot-toast';
 
-export const Toaster = () => (
-  <HotToaster
-    position='bottom-right'
-    toastOptions={{ error: { duration: 5000 } }}
-  />
-);
+export const Toaster = () => <HotToaster position='bottom-right' />;
